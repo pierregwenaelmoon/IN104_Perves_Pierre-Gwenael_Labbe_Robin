53ae417cfe2bb26f007615129611ad512ffcb87b
Pierre-Gwenaël <perves@ensta.fr>
2019-04-01 18:45:12 +0200

    Difficultés à importer OOP dans TDD

--- TDD/tests.py
+++ TDD/tests.py
@@ -1,13 +1,19 @@
 #Test des classes Cartes, Game, et Deck#
 
-import unitest
+import unittest
 
-class TestType(unitest.TestCase)
+from game import Game
 
-	def test_type_carte(CarteTiree):
+class TestType(unittest.TestCase):
+
+	#Test that the carte you pioche is a string#
+
+	def test_type_carte(self):
+		CarteTiree=Game.Pioche()
 		self.assertTrue(type(CarteTiree)==string), "Should be a string"
 
+	
 if __name__=="__main__":
-	test_sum()
+	unittest.main()
 	print("Everything passed")
 
