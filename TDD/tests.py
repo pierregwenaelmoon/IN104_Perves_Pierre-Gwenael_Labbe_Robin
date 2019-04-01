aab189b5acb70d778cce3079f0962108503d5872
Pierre-Gwenaël <perves@ensta.fr>
2019-04-01 19:05:45 +0200

    Rajout du test Nb de Cartes

--- TDD/tests.py
+++ TDD/tests.py
@@ -3,6 +3,8 @@
 import unittest
 
 from game import Game
+from deck import Deck
+from carte import Carte
 
 class TestType(unittest.TestCase):
 
@@ -10,9 +12,19 @@ class TestType(unittest.TestCase):
 
 	def test_type_carte(self):
 		CarteTiree=Game.Pioche()
-		self.assertTrue(type(CarteTiree)==string), "Should be a string"
+		self.assertTrue(type(CarteTiree)==string)
 
-	
+class VerifTaille(unittest.TestCase):
+
+	#Test if the variables are at the good taille#
+
+	#Vérification qu'on a bien toujours le bon nombre de cartes dans le jeu#
+	def test_taille_jeu(self):
+		TailleJeuActuelle=Deck.SizePioche+Deck.SizeFosse+Deck.SizeMain+Deck.SizeMainAdv
+		TailleJeu=Game.NbCards
+		self.assertTrue(TailleJeu==TailleJeuActuelle)
+
+
 if __name__=="__main__":
 	unittest.main()
 	print("Everything passed")
