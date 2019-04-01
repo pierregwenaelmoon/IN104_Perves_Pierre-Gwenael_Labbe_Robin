720a268d7b025742e4e8ce5c3a18d7f9c266ebc9
Pierre-Gwenaël <perves@ensta.fr>
2019-04-01 14:40:23 +0200

    Ajout du dossier TDD

---
+++ TDD/tests.py
@@ -0,0 +1,9 @@
+#Test des classes Cartes, Game, et Deck#
+
+def test_type_carte(CarteTiree):
+	assert type(CarteTiree)==string, "Should be a string"
+
+	if __name__=="__main__":
+		test_sum()
+		print("Everything passed")
+
