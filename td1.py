ad1268e3bb47761f99df415c61e7e5b5135f83d0
Pierre-Gwenael PERVES <perves@yoda08.ensta.fr>
2019-06-20 20:19:19 +0100

    test

---
+++ td1.py
@@ -0,0 +1,30 @@
+class Card_bataille(Deck):
+
+	#Classe définissant une carte d'un jeu de bataille#
+
+	#On construit notre classe#
+
+	def __init__(self):
+		self.num=None
+		self.color=None
+
+	def is_valid(self,game)
+		#Vérifie que la carte existe bien dans le jeu#
+
+	def get_color(self):
+		return(self.color)
+
+	def get_num(self):
+		return(self.num)
+
+
+class Deck:
+
+	#Classe définissant les cartes qui sont posées au milieu#
+
+	def __init__(self):
+		self.NumberOfCards=None
+		self.cards=[]
+
+
+
